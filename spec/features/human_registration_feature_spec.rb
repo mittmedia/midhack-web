7eca4e2857b8f537c3834711347ef781bab49434
Beatrice Nilsson <beatrice.nilsson@mittmedia.se>
2015-08-14 14:34:48 +0200

    TW-473: Writing features.

--- spec/features/human_registration_feature_spec.rb
+++ spec/features/human_registration_feature_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe 'the signup process', type: :feature do
+describe 'the signup process', type: :feature, js: true do
   before(:each) do
     @team = FactoryGirl.create(:team)
     @institution = FactoryGirl.create(:institution)
@@ -15,25 +15,38 @@ describe 'the signup process', type: :feature do
 
     it 'has a button to the what page' do
       visit root_path
-      expect(page).to have_link('Vad är Midhack?', href: what_path)
+      expect(page).to have_link('what_button', href: what_path)
     end
   end
 
   describe 'the visitor visits the what_path' do
     it 'has a signup link' do
       visit what_path
-      expect(page).to have_link('Ja, anmäl mig till Midhack', href: signup_path)
+      expect(page).to have_link('signup_button', href: signup_path)
     end
 
     it 'has a read more link' do
       visit what_path
-      expect(page).to have_link('Jag vill läsa mer')
+      expect(page).to have_link('why_button')
     end
 
     scenario 'the visitor visits the what_path from the root_path' do
       visit root_path
-      click_link 'Vad är Midhack?'
+      click_link 'what_button'
       expect(page).to have_text 'Vad är Midhack'
+    end
+  end
+
+  describe 'the visitor visits the why_path' do
+    it 'has a signup link' do
+      visit why_path
+      expect(page).to have_link('signup_button')
+    end
+
+    scenario 'the visitor visits the signup_path from the what_path' do
+      visit why_path
+      click_link 'signup_button'
+      expect(page).to have_text 'Vad pluggar du?'
     end
   end
 end
