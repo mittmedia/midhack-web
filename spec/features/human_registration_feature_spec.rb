a74aebff9426715df4f2129e703af26fd37f9c70
Beatrice Nilsson <beatrice.nilsson@mittmedia.se>
2015-08-18 08:45:24 +0200

    TW-473: Changed test descriptions.

--- spec/features/human_registration_feature_spec.rb
+++ spec/features/human_registration_feature_spec.rb
@@ -7,7 +7,7 @@ describe 'the signup process', type: :feature do
     @competence = FactoryGirl.create(:competence)
   end
 
-  describe 'The visitor visits the rooth path' do
+  describe 'The visitor visits the root path' do
     it 'has a logo' do
       visit root_path
       expect(page.all('.logo').count).to eq(1)
@@ -19,7 +19,7 @@ describe 'the signup process', type: :feature do
     end
   end
 
-  describe 'the visitor visits the what_path' do
+  describe 'The visitor visits the what_path' do
     it 'has a signup link' do
       visit what_path
       expect(page).to have_link('signup_button', href: signup_path)
@@ -30,20 +30,20 @@ describe 'the signup process', type: :feature do
       expect(page).to have_link('why_button')
     end
 
-    scenario 'the visitor visits the what_path from the root_path' do
+    scenario 'The visitor visits the what_path from the root_path' do
       visit root_path
       click_link 'what_button'
       expect(page).to have_text 'Vad är Midhack'
     end
   end
 
-  describe 'the visitor visits the why_path' do
+  describe 'The visitor visits the why_path' do
     it 'has a signup link' do
       visit why_path
       expect(page).to have_link('signup_button')
     end
 
-    scenario 'the visitor visits the signup_path from the what_path' do
+    scenario 'The visitor visits the signup_path from the what_path' do
       visit why_path
       click_link 'signup_button'
       expect(page).to have_text 'Vad pluggar du?'
@@ -52,22 +52,25 @@ describe 'the signup process', type: :feature do
 
   describe 'The visitor picks course and study year' do
     it 'has more than one course button' do
+  describe 'The visitor starts signing up for the event' do
+    it 'picks a course and study year' do
       visit signup_path
       expect(page).to have_css('div.course_button')
       expect(page).to have_css('div.year', count: 1)
     end
   end
 
-  # describe 'Teams are displayed' do
-  #   it 'has a name' do
-  #     visit signup_path
-  #     expect(page).to have_css('#chooseteam')
-  #     expect(page).to have_text 'Coolt! Vi behöver hackers i dessa lag:'
-  #   end
-  # end
+  describe 'The visitor continuies to sign up' do
+    it 'is presented teams' do
+      visit signup_path
+      click_link('År 1')
+      expect(page).to have_css('#chooseteam')
+      expect(page).to have_text 'Coolt! Vi behöver hackers i dessa lag:'
+    end
+  end
 
-  describe 'Visitor picks a team' do
-    it 'teams are available' do
+  describe 'The visitor finalises the sign up process' do
+    it 'picks a team and provides an email address' do
       visit signup_path
       click_link('År 1')
       click_link('Programmering')
