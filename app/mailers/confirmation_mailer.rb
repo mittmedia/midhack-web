ec428ffdcf47f64ad08f68ae53bce3d6d69ee9eb
Stefan Wallin <stefan.w@festiz.com>
2015-08-19 15:15:53 +0200

    TW-509: Add email categorization.

--- app/mailers/confirmation_mailer.rb
+++ app/mailers/confirmation_mailer.rb
@@ -2,6 +2,8 @@ class ConfirmationMailer < ApplicationMailer
   def confirmation_email(human)
     @human = human
     @url = receipt_url(uuid: human.uuid)
+    # Sendgrid Categorization
+    headers('X-SMTPAPI' => '{"category": "Confirmation"}')
     mail(to: @human.email, subject: 'Du är anmäld till Midhack.')
   end
 end
