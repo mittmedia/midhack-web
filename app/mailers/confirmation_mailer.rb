1d88c4e03e9e1124cd71047eb0ec9640a8df3593
Stefan Wallin <stefan.w@festiz.com>
2015-08-25 17:01:25 +0200

    TW-509: Design the confirmation email.

--- app/mailers/confirmation_mailer.rb
+++ app/mailers/confirmation_mailer.rb
@@ -2,8 +2,10 @@ class ConfirmationMailer < ApplicationMailer
   def confirmation_email(human)
     @human = human
     @url = receipt_url(uuid: human.uuid)
+    @unsubscribe_url = ''
+    @team_name = human.team.name
     # Sendgrid Categorization
     headers('X-SMTPAPI' => '{"category": "Confirmation"}')
-    mail(to: @human.email, subject: 'Du är anmäld till Midhack.')
+    mail(to: @human.email, subject: 'Registration for Midhack')
   end
 end
