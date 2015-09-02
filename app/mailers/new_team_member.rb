ed05ecfcef69c8421ff9606375479f4498db8ae3
Karolin Jonsson <white-sorrow@hotmail.com>
2015-09-02 08:22:16 +0200

    Updating the style of the new member e-mail

--- app/mailers/new_team_member.rb
+++ app/mailers/new_team_member.rb
@@ -1,9 +1,10 @@
 class NewTeamMember < ApplicationMailer
   def new_member_email(human, team_member_details)
+    @human = human
     @team_member_details = team_member_details
-    @url = receipt_url(uuid: human.uuid)
+    @url = why_url
     @unsubscribe_url = quit_url(uuid: @human.uuid)
     headers('X-SMTPAPI' => '{"category": "New Team Member"}')
-    mail(to: human.email, subject: 'Ny medlem i ditt Midhack-lag')
+    mail(to: @human.email, subject: 'Ny medlem i ditt Midhack-lag')
   end
 end
