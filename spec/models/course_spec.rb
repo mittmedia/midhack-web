c0cb99754d5291c5964e83d7380943e1bb1c8233
Stefan Wallin <stefan.w@festiz.com>
2015-09-06 20:17:32 +0200

    Write tests for the course model.

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -17,5 +17,45 @@
 require 'rails_helper'
 
 RSpec.describe Course, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @course = FactoryGirl.create :course
+  end
+  describe 'points' do
+    it 'should not accept 0' do
+      expect(@course.update(points: 0)).to eq(false)
+    end
+    it 'should not be allowed to be nil' do
+      expect(@course.update(points: nil)).to eq(false)
+    end
+    it 'should not be allowed to be negative' do
+      expect(@course.update(points: -10)).to eq(false)
+    end
+    it 'should allow positive integers' do
+      expect(@course.update(points: 10)).to eq(true)
+    end
+  end
+
+  describe 'name' do
+    it 'should be unique to be allowed to persist' do
+      expect(@course.update(name: 'Testkurs')).to eq(true)
+      other_course = FactoryGirl.create :other_course
+      expect(other_course.update(name: 'Testkurs')).to eq(false)
+    end
+    it 'should be be filled to be allowed to persist' do
+      expect(@course.update(name: '')).to eq(false)
+      expect(@course.update(name: nil)).to eq(false)
+    end
+  end
+
+  describe 'code' do
+    it 'should be unique to be allowed to persist' do
+      expect(@course.update(code: 'code')).to eq(true)
+      other_course = FactoryGirl.create :other_course
+      expect(other_course.update(code: 'code')).to eq(false)
+    end
+    it 'should be be filled to be allowed to persist' do
+      expect(@course.update(code: '')).to eq(false)
+      expect(@course.update(code: nil)).to eq(false)
+    end
+  end
 end
