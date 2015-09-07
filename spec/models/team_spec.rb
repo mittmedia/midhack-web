8f0b4bac78cd92a8c942089a3b259ba30fcf4acc
Stefan Wallin <stefan.w@festiz.com>
2015-09-07 22:18:36 +0200

    Fix indentation.

--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -12,22 +12,22 @@ require 'rails_helper'
 
 RSpec.describe Team, type: :model do
   describe 'name' do
-		it 'should be required to be allowed to persist' do
-			expect(Team.create().persisted?).to eq(false)
-			expect(Team.create(name: 'Testteam').persisted?).to eq(true)
-		end
+    it 'should be required to be allowed to persist' do
+      expect(Team.create().persisted?).to eq(false)
+      expect(Team.create(name: 'Testteam').persisted?).to eq(true)
+    end
 
-		it 'should be a unique to be allowed to persist' do
-			expect(Team.create(name: 'Testteam').persisted?).to eq(true)
-			expect(Team.create(name: 'Testteam').persisted?).to eq(false)
-		end
+    it 'should be a unique to be allowed to persist' do
+      expect(Team.create(name: 'Testteam').persisted?).to eq(true)
+      expect(Team.create(name: 'Testteam').persisted?).to eq(false)
+    end
   end
 
   it 'has humans that are not members' do
-  	team = FactoryGirl.create(:team)
-  	h1 = FactoryGirl.create(:human, team: team)
-  	h2 = FactoryGirl.create(:member, team: team)
-  	expect(team.members).to eq([h2])
-  	expect(team.humen).to eq([h1, h2])
+    team = FactoryGirl.create(:team)
+    h1 = FactoryGirl.create(:human, team: team)
+    h2 = FactoryGirl.create(:member, team: team)
+    expect(team.members).to eq([h2])
+    expect(team.humen).to eq([h1, h2])
   end
 end
