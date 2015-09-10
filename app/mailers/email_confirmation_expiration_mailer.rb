141df403249fc1d50a9f6bac5e65b9b82b56d8d9
Karolin Jonsson <white-sorrow@hotmail.com>
2015-09-10 12:56:28 +0200

    tw-661 Adding translation for confirmation expiration e-mail

--- app/mailers/email_confirmation_expiration_mailer.rb
+++ app/mailers/email_confirmation_expiration_mailer.rb
@@ -2,6 +2,6 @@ class EmailConfirmationExpirationMailer < ApplicationMailer
   def time_expired(human)
     # Sendgrid Categorization
     headers('X-SMTPAPI' => '{"category": "Email Confirmation Time Window Expired "}')
-    mail(to: human.email, subject: 'Din tidsfrist har löpt ut')
+    mail(to: human.email, subject: t('.subject'))
   end
 end
