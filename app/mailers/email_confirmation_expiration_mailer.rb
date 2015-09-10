39ae03d880dd47c89957a30cbbf7699c33abd800
Karolin Jonsson <white-sorrow@hotmail.com>
2015-09-10 15:01:04 +0200

    updating and translating e-mails

--- app/mailers/email_confirmation_expiration_mailer.rb
+++ app/mailers/email_confirmation_expiration_mailer.rb
@@ -1,5 +1,6 @@
 class EmailConfirmationExpirationMailer < ApplicationMailer
   def time_expired(human)
+    @team_name = human.team.name
     # Sendgrid Categorization
     headers('X-SMTPAPI' => '{"category": "Email Confirmation Time Window Expired "}')
     human.locale = Rails.configuration.i18n.default_locale if human.locale.blank?
