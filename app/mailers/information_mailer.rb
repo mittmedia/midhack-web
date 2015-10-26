294db387c8048a234a556d5fafc161abe864d502
Stefan Wallin <stefan.w@festiz.com>
2015-10-26 22:09:59 +0100

    TW-759: Add information mailer that tells members that the programme was added to the website

---
+++ app/mailers/information_mailer.rb
@@ -0,0 +1,20 @@
+class InformationMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.information.programme.subject
+  #
+  def programme(human)
+    @receipt_url = receipt_url(uuid: human.uuid)
+    @unsubscribe_url = quit_url(uuid: human.uuid)
+    @programme_url = why_url(uuid: human.uuid)
+
+    # Sendgrid Categorization
+    headers('X-SMTPAPI' => '{"category": "Programme released"}')
+    human.locale = Rails.configuration.i18n.default_locale if human.locale.blank?
+    I18n.with_locale(human.locale) do
+      mail(to: human.email, subject: t('.subject'))
+    end
+  end
+end
---
+++ spec/mailers/information_mailer_spec.rb
@@ -0,0 +1,9 @@
+require "rails_helper"
+
+RSpec.describe InformationMailer, type: :mailer do
+  describe "programme" do
+    let(:mail) { InformationMailer.programme }
+    pending "add some examples to (or delete) #{__FILE__}"
+  end
+
+end
