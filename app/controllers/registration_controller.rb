66716b74d9f2ec6182b87f33f576ef6404418c80
Stefan Wallin <stefan.w@festiz.com>
2016-11-16 16:18:41 +0100

    Add cancellation message

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -1,13 +1,6 @@
 class RegistrationController < ApplicationController
-  before_action :redirect_if_application_date_expired
-  def redirect_if_application_date_expired
-    return redirect_to :signup_closed if application_date_expired?
-  end
-
   def signup
+    return redirect_to :root if application_date_expired?
     @url = Rails.configuration.registration_form_url
-  end
-
-  def signup_closed
   end
 end
